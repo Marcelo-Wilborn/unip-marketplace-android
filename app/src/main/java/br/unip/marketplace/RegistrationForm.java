ae7c73bcf97986605f4fdf6248593a2eaf49f463
Marcelo Wilborn <marcelowb98@gmail.com>
2023-11-30 05:08:51 -0300

    Layout das telas de login, cadastro, usuário e carrinho

---
+++ app/src/main/java/br/unip/marketplace/RegistrationForm.java
@@ -0,0 +1,14 @@
+package br.unip.marketplace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrationForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_form);
+    }
+}
